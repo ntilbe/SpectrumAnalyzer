25fbcd8769d6fe5b9542c7344291d55eb7b8218d
Nicole Tilbe <nicolecali2406@outlook.com>
2020-04-29 20:40:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpectrumAnalyzer
+
+Developed with Unreal Engine 4
---
+++ Source/SpectrumAnalyzer/SpectrumAnalyzerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpectrumAnalyzerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECTRUMANALYZER_API ASpectrumAnalyzerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
